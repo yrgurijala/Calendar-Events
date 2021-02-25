9a2dd8b2d6741dc38cafc7c12f4b6d029dedb2af
yrgurijala <gurijalayashwanth31@gmail.com>
2021-02-24 19:36:50 -0600

    Displays the selected date

---
+++ src/main/java/io/yash/company_portal/entity/CustomDate.java
@@ -0,0 +1,14 @@
+package io.yash.company_portal.entity;
+
+public class CustomDate {
+
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
