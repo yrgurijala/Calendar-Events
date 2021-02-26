5423c5ebb81d00aae32c4c69e14cb288409326c6
yrgurijala <gurijalayashwanth31@gmail.com>
2021-02-26 13:15:25 -0600

    Added a create event option

--- src/main/java/io/yash/company_portal/AppController.java
+++ src/main/java/io/yash/company_portal/AppController.java
@@ -1,7 +1,9 @@
 package io.yash.company_portal;
 
 import io.yash.company_portal.entity.CustomDate;
+import io.yash.company_portal.entity.Event;
 import io.yash.company_portal.entity.User;
+import io.yash.company_portal.repository.EventRepository;
 import io.yash.company_portal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,13 +11,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
 public class AppController {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepo;
+
+    @Autowired
+    private EventRepository eventRepo;
 
     @GetMapping("")
     public String getHomePage(){
@@ -30,13 +36,18 @@ public class AppController {
 
     @PostMapping("/process_registration")
     public String processRegistration(User user){
-        repo.save(user);
+        userRepo.save(user);
         return "registration_success";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome";
     }
 
     @GetMapping("/list_users")
     public String getUserList(Model model){
-        List<User> listUsers = repo.findAll();
+        List<User> listUsers = userRepo.findAll();
         model.addAttribute("listUsers", listUsers);
         return "users";
     }
@@ -53,8 +64,21 @@ public class AppController {
         return "calendar_updated";
     }
 
-    @GetMapping("/welcome")
-    public String welcome(){
-        return "welcome";
+    @GetMapping("/create_event")
+    public String createEvent(Model model){
+        model.addAttribute("event", new Event());
+        return "create_event";
+    }
+
+    @PostMapping("/process_event_creation")
+    public String processEventCreation(Event event, Principal principal, Model model){
+        model.addAttribute("date", new CustomDate());
+
+        String userEmail = principal.getName();
+        User loggedInUser = userRepo.findByEmail(userEmail);
+
+        event.setFullName(loggedInUser.getFirstName() + " " + loggedInUser.getLastName());
+        eventRepo.save(event);
+        return "calendar";
     }
 }
